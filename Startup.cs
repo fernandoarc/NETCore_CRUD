c82ea582d542bb1a0a91975f85ad030dcdf862e2
Fernando Antonio Raihuanque Cid <fernando.arc02@gmail.com>
2022-05-21 19:03:22 -0400

    enrutado - se modifica enrutado automatico https para evitar warnings por no contar con certificado SSL en ambiente local

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,8 @@ namespace CRUD_NetCore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //habilitar redireccionar de manera forzada a ruta https
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
