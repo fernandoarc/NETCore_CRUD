3242295a747588aefb35486b76a80ba56e0c595f
Fernando Antonio Raihuanque Cid <fernando.arc02@gmail.com>
2022-05-23 21:59:46 -0400

    se crea cadena de conexion

--- Models/BD/TurnosContext.cs
+++ Models/BD/TurnosContext.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
 namespace CRUD_NetCore.Models.BD
 {
-    public class TurnosContext
+    public class TurnosContext : DbContext
     {
-        
+        public TurnosContext(DbContextOptions<TurnosContext> opciones) : base(opciones)
+        {
+
+        }
+        public DbSet<Especialidad> Especialidad { get; set; }
     }
 }
