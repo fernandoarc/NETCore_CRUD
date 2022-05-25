556a180b904564cc75457b2cfc9821bf1b0e7942
Fernando Antonio Raihuanque Cid <fernando.arc02@gmail.com>
2022-05-25 19:44:31 -0400

    controlador - se actualiza controlador especialidad para obtener informacion desde la base de datos

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -1,12 +1,14 @@
+using CRUD_NetCore.Models.BD;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRUD_NetCore.Controllers
 {
     public class EspecialidadController : Controller
     {
-        public EspecialidadController()
+        private readonly TurnosContext _context;
+        public EspecialidadController(TurnosContext context)
         {
-
+            _context = context;
         }
 
         public IActionResult Index()
