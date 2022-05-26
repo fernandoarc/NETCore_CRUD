4d307c50e8c4a8f5743a6f2bec5ec4be87548743
Fernando Antonio Raihuanque Cid <fernando.arc02@gmail.com>
2022-05-25 20:53:11 -0400

    controlador - se crea metodo encargado de editar especialidad

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -1,3 +1,4 @@
+using CRUD_NetCore.Models;
 using System.Linq;
 using CRUD_NetCore.Models.BD;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +16,36 @@ namespace CRUD_NetCore.Controllers
         public IActionResult Index()
         {
             return View(_context.Especialidad.ToList());
+        }
+
+        public IActionResult Editar(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+            var especialidad = _context.Especialidad.Where(e => e.IdEspecialidad == Id).FirstOrDefault();
+            if(especialidad == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int? id, [Bind("IdEspecialidad,Descripcion")] Especialidad especialidad)
+        {
+            if (id != especialidad.IdEspecialidad)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(especialidad);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
         }
     }
 }
