df3ff4249da6fb99f51d3a1f0f1ed7850dcb05e7
Fernando Raihuanque <fernando.arc02@gmail.com>
2022-06-08 22:42:54 -0400

    entity - se sobre escribe clase on model creating para especificar mayor nivel de detalle creacion de tablas al interior sql server

--- Models/BD/TurnosContext.cs
+++ Models/BD/TurnosContext.cs
@@ -8,5 +8,18 @@ namespace CRUD_NetCore.Models.BD
 
         }
         public DbSet<Especialidad> Especialidad { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //configuraciones a implementar al momento de crear tabla en el sql server
+            modelBuilder.Entity<Especialidad>(entidad => {
+                entidad.ToTable("Especialidad");//nombre de la tabla en sql server
+                entidad.HasKey(e => e.IdEspecialidad);//especificando primary key
+                entidad.Property(e => e.Descripcion)
+                    .IsRequired()//campo obligatorio
+                    .HasMaxLength(200)//largo maximo
+                    .IsUnicode(false);//campo unicode
+            });
+        }
     }
 }
